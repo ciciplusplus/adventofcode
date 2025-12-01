9770ac2b1aedb6098f0b673d570c9efa1f0d79c6
ciciplusplus <ciciplusplus@gmail.com>
2025-12-01 20:57:40 +0100

    2025: day 1, part 2

--- 2025/aoc/src/main.rs
+++ 2025/aoc/src/main.rs
@@ -8,6 +8,7 @@ fn main() {
     let filename = args().nth(1).unwrap();
     for line in read_to_string(filename).unwrap().lines() {
         let amount = line[1..].parse::<i32>().unwrap();
+        turns_count += amount / 100;
         let amount = amount % 100;
         let to_turn = match line.chars().nth(0).unwrap() {
             'R' => amount,
@@ -17,13 +18,12 @@ fn main() {
         let mut current = start + to_turn;
         if current < 0 {
             current = current + 100;
+            turns_count += 1;
         } else if current > 99 {
             current = current % 100;
-        }
-        start = current;
-        if start == 0 {
             turns_count += 1;
         }
+        start = current;
     }
 
     print!("pass {}", turns_count);
