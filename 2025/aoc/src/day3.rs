a95d99d5c74bd21711dc5d19cd2a8d4130252572
ciciplusplus <ciciplusplus@gmail.com>
2025-12-03 22:28:23 +0100

    2025: day 3, part 2

--- 2025/aoc/src/day3.rs
+++ 2025/aoc/src/day3.rs
@@ -1,30 +1,36 @@
 use std::fs::read_to_string;
 
-fn get_max(str: &str, start_idx: usize, end_idx: usize) -> (usize, u64) {
-    let mut max_idx = start_idx;
-    let mut max_val = str.chars().nth(start_idx).unwrap();
-    for i in start_idx..=end_idx {
-        let curr = str.chars().nth(i).unwrap();
-        if curr > max_val {
-            max_idx = i;
-            max_val = curr;
-        }
-    }
-    (max_idx, max_val.to_digit(10).unwrap().into())
-}
+/*
+
+i [0, 12]
+
+j [0, str.len)
+
+a(i, j) = max(str[j] + a(i-1, j-1), a(i, j-1));
+ */
+
+const SIZE: usize = 12;
 
 pub fn day3(filename: &str) -> u64 {
     let mut ans = 0;
     for x in read_to_string(filename).unwrap().lines() {
-        let (first_idx, first) = get_max(x, 0, x.len()-1);
-        let max = if first_idx == x.len()-1 {
-            let (_, before) = get_max(x, 0, first_idx-1);
-            before * 10 + first
-        } else {
-            let (_, second) = get_max(x, first_idx+1, x.len()-1);
-            first * 10 + second
-        };
-        ans += max;
+        let mut dp1: [u64; SIZE] = [0; SIZE];
+        let mut dp: [u64; SIZE] = [0; SIZE];
+
+        for j in (0..x.len()).rev() {
+            let c: u64 = x.chars().nth(j).unwrap().to_digit(10).unwrap().into();
+            for i in 0..dp.len().min(x.len() - j) {
+                if i == 0 {
+                    dp[i] = dp1[i].max(c);
+                } else {
+                    dp[i] = dp1[i].max(c * 10u64.pow(i as u32) + dp1[i - 1]);
+                }
+            }
+            dp1.clone_from_slice(&dp);
+        }
+        //println!("{}", dp[dp.len() - 1]);
+
+        ans += dp[dp.len() - 1];
     }
     ans
 }
