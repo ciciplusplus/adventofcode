f69ecd500072a2ea32e1a898c4c87ff4b1e508e7
ciciplusplus <ciciplusplus@gmail.com>
2025-12-04 08:16:43 +0100

    2025: day 4, part 1

---
+++ 2025/aoc/src/day4.rs
@@ -0,0 +1,42 @@
+use std::fs::read_to_string;
+
+pub fn day4(filename: &str) -> u64 {
+    let mut ans = 0;
+    let mut grid: Vec<Vec<char>> = Vec::new();
+    for x in read_to_string(filename).unwrap().lines() {
+        grid.push(x.chars().collect());
+    }
+    //let mut grid1 = grid.clone();
+    let n: i32 = grid.len().try_into().unwrap();
+    let m: i32 = grid[0].len().try_into().unwrap();
+    for i in 0..n {
+        for j in 0..m {
+            if grid[i as usize][j as usize] == '.' {
+                continue;
+            }
+            let mut count = 0;
+            for p in -1..=1 {
+                if !(0 <= i+p && i+p < n) {
+                    continue;
+                }
+                for q in -1..=1 {
+                    if p == 0 && q == 0 {
+                        continue;
+                    }
+                    if !(0 <= j+q && j+q < m) {
+                        continue;
+                    }
+                    if grid[(i+p) as usize][(j+q) as usize] == '@' {
+                        count += 1;
+                    }
+                }
+            }
+            if count < 4 {
+                ans += 1;
+                //grid1[i as usize][j as usize] = 'x';
+            }
+        }
+    }
+    //println!("{:?}", grid1);
+    ans
+}
--- 2025/aoc/src/main.rs
+++ 2025/aoc/src/main.rs
@@ -3,11 +3,13 @@ use std::env::args;
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 
 fn main() {
     let filename = args().nth(1).unwrap();
     //let res = day1::day1(&filename);
     //let res = day2::day2(&filename);
-    let res = day3::day3(&filename);
+    //let res = day3::day3(&filename);
+    let res = day4::day4(&filename);
     print!("res {}", res);
 }
