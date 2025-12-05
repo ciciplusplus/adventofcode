a92d32a0746cf23699fb7a0f7bcd768bc284ca3e
ciciplusplus <ciciplusplus@gmail.com>
2025-12-05 13:31:32 +0100

    2025: day 5, part 2

--- 2025/aoc/src/day5.rs
+++ 2025/aoc/src/day5.rs
@@ -2,7 +2,7 @@ use std::fs::read_to_string;
 use std::ops::Range;
 
 pub fn day5(filename: &str) -> u64 {
-    let mut ranges = Vec::new();
+    let mut ranges: Vec<Range<u64>> = Vec::new();
     let binding = read_to_string(filename).unwrap();
     let mut lines = binding.lines();
     while let Some(line) = lines.next() {
@@ -10,18 +10,23 @@ pub fn day5(filename: &str) -> u64 {
             break;
         }
         let (l, r) = line.split_once("-").unwrap();
-        let r = Range { start: l.parse::<u64>().unwrap(), end: r.parse::<u64>().unwrap() + 1 };
-        ranges.push(r);
-    }
-    let mut count = 0;
-    while let Some(line) = lines.next() {
-        let num = line.parse::<u64>().unwrap();
-        for r in ranges.clone() { // DING! DING! DING! clone alert!
-            if r.contains(&num) {
-                count += 1;
-                break
+        let mut r = Range { start: l.parse::<u64>().unwrap(), end: r.parse::<u64>().unwrap() + 1 };
+        let mut i = 0;
+        while i < ranges.len() {
+            let rr = &ranges[i];
+            if rr.contains(&r.start) || r.contains(&rr.start) {
+                r.start = r.start.min(rr.start);
+                r.end = r.end.max(rr.end);
+                ranges.remove(i);
+            } else {
+                i += 1;
             }
         }
+        ranges.push(r);
+    }
+    let mut count: u64 = 0;
+    for r in ranges {
+        count += r.count() as u64;
     }
     count
 }
