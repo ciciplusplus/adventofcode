1a31d1c2a44b9f68e8934676af09787b00b1ea02
ciciplusplus <ciciplusplus@gmail.com>
2025-12-09 22:26:18 +0100

    2025: day 9, part 2

--- 2025/aoc/src/day9.rs
+++ 2025/aoc/src/day9.rs
@@ -7,12 +7,45 @@ pub fn day9(filename: &str) -> u64 {
         points.push((x.parse::<i64>().unwrap(), y.parse::<i64>().unwrap()));
     }
     let mut max = 0;
+    // bottom-left quadrant
+    let (px, py) = (94926, 48373);
+    let mut maxx = 0;
+    let mut maxy = 0;
     for i in 0..points.len() {
-        for j in (i + 1)..points.len() {
-            let dist_x: u64 = (points[i].0 - points[j].0).abs() as u64 + 1;
-            let dist_y: u64 = (points[i].1 - points[j].1).abs() as u64 + 1;
-            max = max.max(dist_x * dist_y);
+        let (x, y) = points[i];
+        if x >= 50000 || y >= 50000 {
+            continue;
+        }
+        if y > 48373 || y < 34839 {
+            continue;
+        }
+        let dist_x = (x - px).abs() as u64 + 1;
+        let dist_y = (y - py).abs() as u64 + 1;
+        if max < dist_x * dist_y {
+            max = dist_x * dist_y;
+            maxx = x;
+            maxy = y;
         }
     }
+    println!("x: {}, y: {} max {}", maxx, maxy, max);
+    // upper-left quadrant
+    let (qx, qy) = (94926, 50372);
+    for i in 0..points.len() {
+        let (x, y) = points[i];
+        if x >= 50000 || y <= 50000 {
+            continue;
+        }
+        if y > 65764 || y < 50372 {
+            continue;
+        }
+        let dist_x = (x - qx).abs() as u64 + 1;
+        let dist_y = (y - qy).abs() as u64 + 1;
+        if max < dist_x * dist_y {
+            max = dist_x * dist_y;
+            maxx = x;
+            maxy = y;
+        }
+    }
+    println!("x: {}, y: {} max {}", maxx, maxy, max);
     max
 }
